5bb1dbb0f72a22afee911c9b744eaf06fbb21671
Krystian Otto <otto.krystiann@gmail.com>
2021-01-05 18:40:46 +0100

    add react-hook-form, add simple form that send new Issue for Api

--- client/src/components/pages/NewIssue/NewIssue.js
+++ client/src/components/pages/NewIssue/NewIssue.js
@@ -1,9 +1,46 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { useDispatch } from 'react-redux';
+import { addNewIssue } from '../../../redux/issuesRedux';
 
 const NewIssue = () => {
+  const { register, handleSubmit } = useForm({ mode: 'onChange' });
+  const dispatch = useDispatch();
+
+  const submitForm = (data) => {
+    console.log(data);
+    dispatch(addNewIssue(data));
+  };
+
   return (
     <>
-      <p>New Issue Form</p>
+      <form onSubmit={handleSubmit(submitForm)}>
+        <input
+          type="text"
+          placeholder="title"
+          name="title"
+          ref={register({
+            required: true,
+          })}
+        />
+        <input
+          type="text"
+          placeholder="author"
+          name="author"
+          ref={register({
+            required: true,
+          })}
+        />
+        <input
+          type="text"
+          placeholder="description"
+          name="description"
+          ref={register({
+            required: true,
+          })}
+        />
+        <button type="submit">Add Issue</button>
+      </form>
     </>
   );
 };
--- client/src/redux/issuesRedux.js
+++ client/src/redux/issuesRedux.js
@@ -5,7 +5,10 @@ const reducerName = 'issues';
 const createActionName = (name) => `app/${reducerName}/${name}`;
 
 const LOAD_ISSUES = createActionName('LOAD_ISSUES');
+const ADD_ISSUE = createActionName('ADD_ISSUE');
+
 export const loadIssues = (payload) => ({ payload, type: LOAD_ISSUES });
+export const addIssue = (payload) => ({ payload, type: ADD_ISSUE });
 
 /* THUNKS */
 
@@ -13,6 +16,13 @@ export const fetchIssuesFromApi = () => {
   return async (dispatch) => {
     let res = await axios.get('http://localhost:8000/api/issues');
     dispatch(loadIssues(res.data));
+  };
+};
+
+export const addNewIssue = (data) => {
+  return async (dispatch) => {
+    let res = await axios.post('http://localhost:8000/api/issues', data);
+    dispatch(addIssue(res));
   };
 };
 
