f597a7f704d39d0c586536c9f2599011f3dcda68
Krystian Otto <otto.krystiann@gmail.com>
2021-01-06 12:27:14 +0100

    add styles for IssuesList

--- client/src/components/pages/IssuesList/IssuesList.js
+++ client/src/components/pages/IssuesList/IssuesList.js
@@ -38,23 +38,25 @@ const IssuesList = () => {
 
   return (
     <div>
-      {issues ? (
-        issues.map(({ _id, title, date, author, tags }) => (
-          <div key={_id}>
-            <StyledLink to={`/issue/${_id}`}>
-              <h3>{title}</h3>
-            </StyledLink>
-            <h4>
-              #{_id} opened on {dateFormat(date)} by {author}
-            </h4>
-            {tags.map((tag) => (
-              <p key={tag}>{tag}</p>
-            ))}
-          </div>
-        ))
-      ) : (
-        <span>No Issues Found!</span>
-      )}
+      <IssuesContainer>
+        {issues ? (
+          issues.map(({ _id, title, date, author, tags }) => (
+            <IssueContainer key={_id}>
+              <TitleAndTagsContainer>
+                <StyledLink to={`/issue/${_id}`}>{title}</StyledLink>
+                {tags.map((tag) => (
+                  <StyledTag key={tag}>{tag}</StyledTag>
+                ))}
+              </TitleAndTagsContainer>
+              <StyledSpan>
+                #{_id} opened on {dateFormat(date)} by {author}
+              </StyledSpan>
+            </IssueContainer>
+          ))
+        ) : (
+          <span>No Issues Found!</span>
+        )}
+      </IssuesContainer>
       <div>
         <Link to="/issue/new">New Issue</Link>
       </div>
@@ -62,8 +64,42 @@ const IssuesList = () => {
   );
 };
 
+const IssuesContainer = styled.div`
+  border: 1px solid #30363d;
+  width: 80%;
+  margin: auto;
+`;
+
+const IssueContainer = styled.div`
+  border-top: 1px solid #30363d;
+  padding: 1rem;
+
+  :hover {
+    background-color: #161b22;
+  }
+`;
+
+const TitleAndTagsContainer = styled.div`
+  padding-bottom: 0.5rem;
+`;
+
 const StyledLink = styled(Link)`
-  display: block;
+  display: inline;
+`;
+
+const StyledSpan = styled.span`
+  font-size: 0.8rem;
+`;
+
+const StyledTag = styled.span`
+  font-size: 0.8rem;
+  color: #00e6c4;
+  padding: 0 7px;
+  border-radius: 2rem;
+  cursor: pointer;
+  border: 1px solid #00e6c4;
+  background-color: #0086722e;
+  margin: 0 0.3rem;
 `;
 
 export default IssuesList;
--- client/src/styles/globalStyle.js
+++ client/src/styles/globalStyle.js
@@ -3,6 +3,7 @@ import { createGlobalStyle } from 'styled-components';
 const GlobalStyle = createGlobalStyle`
   *, *::before, *::after {
     box-sizing: border-box;
+    font-family: 'Roboto', sans-serif;
   }
 
   body {
